088cde4622ad590b12bad7191114f47fd160b3b5
PROGRAMMING-COURSZ <mrmadyt2008@gmail.com>
2025-04-09 20:17:46 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,11 +1,12 @@
 import os
 import logging
 import logging.config
-
+from pyrogram.errors import BadMsgNotification
 # Get logging configurations
 logging.getLogger().setLevel(logging.ERROR)
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
-
+import time
+time.time()  # forces Python to sync system time
 from pyromod import listen
 from pyrogram import Client
 from config import *
@@ -19,7 +20,13 @@ def main():
                  plugins=plugins,
                  workers=100)
 
-    app.run()
+    while True:
+    try:
+        app.run()
+        break
+    except BadMsgNotification:
+        print("Time sync issue, retrying in 10 seconds...")
+        time.sleep(10)
 
 
 if __name__ == "__main__":
